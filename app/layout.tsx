c0079da30e3f94c9db75e4f5c5d96d70e52711e7
Arik Bidny <arikbidny@gmail.com>
2023-05-27 22:27:08 +0300

    Adding modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
-import { Nunito } from 'next/font/google';
+import { Nunito } from "next/font/google";
 
-import './globals.css'
-import Navbar from './components/navbar/Navbar';
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal isOpen />
         <Navbar />
         {children}
-       </body>
+      </body>
     </html>
-  )
+  );
 }
